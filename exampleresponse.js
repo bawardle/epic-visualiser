d9541b4d26857de35e9c06bfe5b6965f2ecbc788
Wardle <benjamin_wardle@next.co.uk>
2025-07-29 10:29:32 +0100

    Initial commit: Set up user story visualizer, excluding server module

---
+++ exampleresponse.js
@@ -0,0 +1,220 @@
+{
+    "user_stories": [
+        {
+            "Id": "4489734",
+            "Name": "[CMP] F04 US2: Stagger CMP Shift Visibility by Employee Star Rating",
+            "State": "Ready for Development",
+            "Iteration": "HR.Tech\\Wednesday - Tuesday\\2025\\AUT25\\SprintAUT25.1",
+            "Precursor": null,
+            "Successor": null
+        },
+        {
+            "Id": "4498198",
+            "Name": "[CMP] F04 US1: Limit the CMP shifts displayed based on department",
+            "State": "Ready for Development",
+            "Iteration": "HR.Tech\\Wednesday - Tuesday\\2025\\SUM25\\SprintSUM25.12",
+            "Precursor": null,
+            "Successor": null
+        },
+        {
+            "Id": "4476685",
+            "Name": "[CMP] F03 US2a: Allow CMP Shift to be added to Vacancies after Advertised End Date",
+            "State": "Development",
+            "Iteration": "HR.Tech\\Wednesday - Tuesday\\2025\\SUM25\\SprintSUM25.13",
+            "Precursor": null,
+            "Successor": null
+        },
+        {
+            "Id": "4457956",
+            "Name": "[CMP] F02:- Retail Super User Access",
+            "State": "Development",
+            "Iteration": "HR.Tech\\Wednesday - Tuesday\\2025\\SUM25\\SprintSUM25.13",
+            "Precursor": null,
+            "Successor": null
+        },
+        {
+            "Id": "4460523",
+            "Name": "[CMP] F02: US6: Contract Marketplace screen - Shifts",
+            "State": "Development",
+            "Iteration": "HR.Tech\\Wednesday - Tuesday\\2025\\SUM25\\SprintSUM25.13",
+            "Precursor": null,
+            "Successor": null
+        },
+        {
+            "Id": "4460118",
+            "Name": "[CMP] F02: US2: SAM CMP to only be displayed to specific Grade Employees",
+            "State": "Ready for Development",
+            "Iteration": "HR.Tech\\Wednesday - Tuesday\\2025\\SUM25\\SprintSUM25.13",
+            "Precursor": null,
+            "Successor": null
+        },
+        {
+            "Id": "4460141",
+            "Name": "[CMP] F02 US3: SAM CMP not be available to Pending Workers",
+            "State": "Development",
+            "Iteration": "HR.Tech\\Wednesday - Tuesday\\2025\\SUM25\\SprintSUM25.12",
+            "Precursor": null,
+            "Successor": null
+        },
+        {
+            "Id": "4421885",
+            "Name": "NASA : In NASA Fix the Username and password embedded in the config files by moving them to password pot and make sure it is not affecting any functionality",
+            "State": "Development",
+            "Iteration": "HR.Tech\\Wednesday - Tuesday\\2025\\AUT25\\SprintAUT25.1",
+            "Precursor": null,
+            "Successor": null
+        },
+        {
+            "Id": "4460587",
+            "Name": "[CMP] F02: US8: Current and Future Contracts",
+            "State": "Development",
+            "Iteration": "HR.Tech\\Wednesday - Tuesday\\2025\\SUM25\\SprintSUM25.11",
+            "Precursor": null,
+            "Successor": null
+        },
+        {
+            "Id": "4488495",
+            "Name": "[CMP] F03 US2b: Update Key in NPS",
+            "State": "Development",
+            "Iteration": "HR.Tech\\Wednesday - Tuesday\\2025\\SUM25\\SprintSUM25.13",
+            "Precursor": null,
+            "Successor": null
+        },
+        {
+            "Id": "4455541",
+            "Name": "[CMP] F01:US6: Assigning shifts advertised on Contract Marketplace",
+            "State": "Development",
+            "Iteration": "HR.Tech\\Wednesday - Tuesday\\2025\\SUM25\\SprintSUM25.12",
+            "Precursor": null,
+            "Successor": null
+        },
+        {
+            "Id": "4476680",
+            "Name": "[CMP] F03 US1: Prevent CMP Shifts from being added to a vacancy if the Advertised End Date is in the future",
+            "State": "Development",
+            "Iteration": "HR.Tech\\Wednesday - Tuesday\\2025\\SUM25\\SprintSUM25.13",
+            "Precursor": null,
+            "Successor": null
+        },
+        {
+            "Id": "4398734",
+            "Name": "ROTA: Fix Inconsistent Errors and Overlap Conflicts in Absence Handling",
+            "State": "Development",
+            "Iteration": "HR.Tech\\Wednesday - Tuesday\\2025\\AUT25\\SprintAUT25.1",
+            "Precursor": null,
+            "Successor": null
+        },
+        {
+            "Id": "4477622",
+            "Name": "[CMP] F03 US3: Prevent shifts from being reserved if they are on CMP and the Advertised End Date is in the future or current day",
+            "State": "Ready for Development",
+            "Iteration": "HR.Tech\\Wednesday - Tuesday\\2025\\AUT25\\SprintAUT25.1",
+            "Precursor": null,
+            "Successor": null
+        },
+        {
+            "Id": "4477624",
+            "Name": "[CMP] F03 US4: Allow shifts to be reserved if they are on CMP and the Advertised End Date is in the past",
+            "State": "Ready for Development",
+            "Iteration": "HR.Tech\\Wednesday - Tuesday\\2025\\AUT25\\SprintAUT25.1",
+            "Precursor": null,
+            "Successor": null
+        },
+        {
+            "Id": "4482029",
+            "Name": "[CMP] F03 US5: Remove Shifts from Contract Marketplace functionality in NPS",
+            "State": "Ready for Development",
+            "Iteration": "HR.Tech\\Wednesday - Tuesday\\2025\\AUT25\\SprintAUT25.1",
+            "Precursor": null,
+            "Successor": null
+        },
+        {
+            "Id": "4455589",
+            "Name": "[CMP] F01:US7: Removing shifts from Contract Marketplace when a leaver record is reversed",
+            "State": "Development",
+            "Iteration": "HR.Tech\\Wednesday - Tuesday\\2025\\SUM25\\SprintSUM25.12",
+            "Precursor": null,
+            "Successor": null
+        },
+        {
+            "Id": "4460153",
+            "Name": "[CMP] F02: US4: Show available Contract Marketplace shifts banner on SAM Calendar",
+            "State": "Development",
+            "Iteration": "HR.Tech\\Wednesday - Tuesday\\2025\\SUM25\\SprintSUM25.13",
+            "Precursor": null,
+            "Successor": null
+        },
+        {
+            "Id": "4440180",
+            "Name": "[CMP] F01:US4: Advertised shifts to be updated to reflect changes made by O&M",
+            "State": "Development",
+            "Iteration": "HR.Tech\\Wednesday - Tuesday\\2025\\SUM25\\SprintSUM25.12",
+            "Precursor": null,
+            "Successor": null
+        },
+        {
+            "Id": "4440166",
+            "Name": "[CMP] F01:US1: Automatically advertise leavers ideal shifts Contract Marketplace",
+            "State": "Development",
+            "Iteration": "HR.Tech\\Wednesday - Tuesday\\2025\\SUM25\\SprintSUM25.12",
+            "Precursor": null,
+            "Successor": null
+        },
+        {
+            "Id": "4460103",
+            "Name": "[CMP] F02: US1: SAM CMP Functionality Switched ON/OFF by Business Area",
+            "State": "Development",
+            "Iteration": "HR.Tech\\Wednesday - Tuesday\\2025\\SUM25\\SprintSUM25.12",
+            "Precursor": null,
+            "Successor": null
+        },
+        {
+            "Id": "4460206",
+            "Name": "[CMP] F02: US5: Contract Marketplace screen",
+            "State": "Development",
+            "Iteration": "HR.Tech\\Wednesday - Tuesday\\2025\\SUM25\\SprintSUM25.11",
+            "Precursor": null,
+            "Successor": null
+        },
+        {
+            "Id": "4460567",
+            "Name": "[CMP] F02: US7: SAM - My Calendar",
+            "State": "Development",
+            "Iteration": "HR.Tech\\Wednesday - Tuesday\\2025\\SUM25\\SprintSUM25.11",
+            "Precursor": null,
+            "Successor": null
+        },
+        {
+            "Id": "4440178",
+            "Name": "[CMP] F01:US3: Facility to Switch Contract Marketplace ON/OFF at Store Level",
+            "State": "Development",
+            "Iteration": "HR.Tech\\Wednesday - Tuesday\\2025\\SUM25\\SprintSUM25.12",
+            "Precursor": null,
+            "Successor": null
+        },
+        {
+            "Id": "4464483",
+            "Name": "[CMP] F02: US7a: SAM - CMP Key",
+            "State": "Development",
+            "Iteration": "HR.Tech\\Wednesday - Tuesday\\2025\\SUM25\\SprintSUM25.11",
+            "Precursor": null,
+            "Successor": null
+        },
+        {
+            "Id": "4466894",
+            "Name": "DESIGN: WFP E02 F03P TUS4 - Rota Service Endpoints - Map Internal methods to main endpoints for the new Web API",
+            "State": "Development",
+            "Iteration": "HR.Tech\\Wednesday - Tuesday\\2025\\SUM25\\SprintSUM25.12",
+            "Precursor": null,
+            "Successor": null
+        },
+        {
+            "Id": "4440",
+            "Name": "1 - Create location for Team to drop in the Item Master",
+            "State": "Closed",
+            "Iteration": "Lipsy.ReportingSystems\\Iteration 1",
+            "Precursor": null,
+            "Successor": null
+        }
+    ]
+}
